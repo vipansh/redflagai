b449a61d0b3f6070de06cde56dfd0a56cc5b9d27
vipansh <vipanshthakur@gmail.com>
2023-03-07 22:03:49 +0530

    🐞 fix:css issue in hero section index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Stripe from "stripe";
 import { Navbar, SignInWithGoogleButton } from "../components";
 import Image from "next/image";
 import redglagAppImage from "public/RedflagApp.png";
+import { motion } from "framer-motion";
 import backgroundImage from "public/background.jpg";
 interface Props {
   products: Stripe.Price[];
@@ -19,6 +20,7 @@ const Home = ({ products }: Props) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar products={products} />
+
       <section className="relative py-10 overflow-hidden bg-slate-50 sm:py-32 hero-pattern">
         <Image
           alt=""
@@ -31,28 +33,36 @@ const Home = ({ products }: Props) => {
         />
 
         <div className="container items-center max-w-6xl px-8 mx-auto xl:px-5">
-          <div className="flex flex-wrap items-center sm:-mx-3">
+          <div className="flex flex-wrap items-start sm:-mx-3 justify-start">
             <div className="w-full md:w-1/2 md:px-3">
               <div className="w-full pb-6 space-y-6 sm:max-w-md lg:max-w-lg md:space-y-4 lg:space-y-8 xl:space-y-9 sm:pr-5 lg:pr-0 md:pb-0">
-                <h1 className="my-4 text-3xl md:text-5xl  opacity-75 font-bold leading-tight text-center md:text-left">
+                <motion.h1
+                  initial={{ rotate: 0 }}
+                  animate={{ rotate: -4 }}
+                  transition={{ duration: 1 }}
+                  className="my-4 text-3xl md:text-5xl  opacity-75 font-bold leading-tight text-center md:text-left"
+                >
                   Revolutionize your legal document review process with -
                   <span className="mx-1 bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500">
                     ReadFlagAI
                   </span>
-                </h1>
+                </motion.h1>
                 <p className="leading-normal text-base md:text-2xl mb-8 text-center md:text-left">
                   the AI-powered tool that detects red flags in terms and
-                  conditions.!
+                  conditions!
                 </p>
                 {!loading && (
-                  <div className="relative flex flex-col sm:flex-row sm:space-x-4 justify-center">
+                  <div className="relative flex flex-col sm:flex-row sm:space-x-4 ">
                     <SignInWithGoogleButton />
                   </div>
                 )}
               </div>
             </div>
             <div className="w-full md:w-1/2 relative">
-              <Image
+              <motion.img
+                initial={{ rotate: 0 }}
+                animate={{ rotate: 4 }}
+                transition={{ duration: 1 }}
                 className=" absolute top-0 left-0 object-cover object-left-top"
                 alt="redflag AI"
                 {...redglagAppImage}
