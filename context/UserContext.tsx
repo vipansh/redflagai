5440b28356e5e0e67aeb9cfa3912e70d62e44a9f
vipansh <vipanshthakur@gmail.com>
2023-03-08 00:29:38 +0530

    fix:redirect🐞 fix: getURL  env NEXT_PUBLIC_VERCEL_URL added

--- context/UserContext.tsx
+++ context/UserContext.tsx
@@ -36,15 +36,7 @@ export const UserProvider: FC<UserProviderProps> = ({ children }) => {
   };
 
   const getURL = () => {
-    let url =
-      process?.env?.NEXT_PUBLIC_SITE_URL || // Set this to your site URL in production env.
-      `${process?.env?.NEXT_PUBLIC_VERCEL_URL}/dashboard/check/` || // Automatically set by Vercel.
-      "https://redflagai.vercel.app/dashboard/check/";
-    // Make sure to include `https://` when not localhost.
-    url = url.includes("http") ? url : `https://${url}`;
-    // Make sure to including trailing `/`.
-    url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
-
+    let url = "https://redflagai.vercel.app/dashboard/check/";
     return url;
   };
 
