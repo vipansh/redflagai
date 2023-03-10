5de23e627680a5c73d62198dcd68f58f8c349aa9
vipansh <vipanshthakur@gmail.com>
2023-03-11 01:12:32 +0530

    🐞fix:remove unused import

--- pages/dashboard/check.tsx
+++ pages/dashboard/check.tsx
@@ -1,11 +1,7 @@
 import axios from "axios";
 import { AnimatePresence, motion } from "framer-motion";
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
 import { useEffect, useState } from "react";
 import { Toaster, toast } from "react-hot-toast";
-import Stripe from "stripe";
 import {
   Navbar,
   ResizablePanel,
