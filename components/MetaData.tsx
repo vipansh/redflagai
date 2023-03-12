3e350aed6f08295a7e8a775f5de151d57c1c65e4
vipansh <vipanshthakur@gmail.com>
2023-03-12 11:19:33 +0530

    🧪 feat:update mata data

--- components/MetaData.tsx
+++ components/MetaData.tsx
@@ -8,41 +8,32 @@ type Props = {
 };
 
 const MetaData: React.FC<Props> = ({
-  title = "RedflagAI",
-  description = "RedFlag AI is a powerful tool that helps identify potential issues in your terms and conditions. With advanced machine learning algorithms, RedFlag AI highlight any problematic areas, allowing you to make informed decisions and minimize risk. Protect your business with RedFlag AI today.",
+  title = "RedFlag AI Terms and Conditions Review | RedflagAI",
+  description = "Looking for a powerful tool to help identify potential issues in your terms and conditions? Look no further than RedFlag AI. Our advanced machine learning algorithms highlight problematic areas, allowing you to make informed decisions and minimize risk. Protect your business with RedFlag AI today.",
 }) => {
   return (
     <Head>
       <title>{title}</title>
       <meta name="description" content={description} />
       <link rel="icon" href={fire.src} />
-      <meta name="image" content={redflagai.src} />
-      <title>RedflagAI</title>
+     
       <meta
-        name="description"
-        content="RedFlag AI is a powerful tool that helps identify potential issues in your terms and conditions. With advanced machine learning algorithms, RedFlag AI highlight any problematic areas, allowing you to make informed decisions and minimize risk. Protect your business with RedFlag AI today."
+        name="keywords"
+        content="RedFlag AI, Terms and Conditions, Review, Machine Learning, Risk Management, Business Protection"
       />
 
-      {/* <!-- Facebook Meta Tags --> */}
-      <meta property="og:url" content="https://www.redflagai.in/" />
+      {/* Open Graph / Facebook */}
       <meta property="og:type" content="website" />
-      <meta property="og:title" content="RedflagAI" />
-      <meta
-        property="og:description"
-        content="RedFlag AI is a powerful tool that helps identify potential issues in your terms and conditions. With advanced machine learning algorithms, RedFlag AI highlight any problematic areas, allowing you to make informed decisions and minimize risk. Protect your business with RedFlag AI today."
-      />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
       <meta property="og:image" content={redflagai.src} />
 
-      {/* <!-- Twitter Meta Tags --> */}
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta property="twitter:domain" content="redflagai.in" />
-      <meta property="twitter:url" content="https://www.redflagai.in/" />
-      <meta name="twitter:title" content="RedflagAI" />
-      <meta
-        name="twitter:description"
-        content="RedFlag AI is a powerful tool that helps identify potential issues in your terms and conditions. With advanced machine learning algorithms, RedFlag AI highlight any problematic areas, allowing you to make informed decisions and minimize risk. Protect your business with RedFlag AI today."
-      />
-      <meta name="twitter:image" content={redflagai.src} />
+      {/* Twitter */}
+      <meta property="twitter:card" content="summary_large_image" />
+
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description} />
+      <meta property="twitter:image" content={redflagai.src} />
     </Head>
   );
 };
